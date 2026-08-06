82a2c0e980fb12e323ec1db7ad0a311feb4ef8dd
DielsonFreitas <dielson.freitas@hotmail.com>
2026-08-05 22:26:06 -0300

    Aula2

---
+++ AULA2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	float A, B, C, MEDIA;
+	
+	
+	A = 5.8;
+	B = 7.6;
+	C = 6.7;
+	
+	MEDIA = (A+B+C) / 2;
+	
+	printf("A média será de: %2.f\n", MEDIA);
+	
+	
+	
+	return 0;
+}

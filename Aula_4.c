4520fb854164ea41781a6006c3dfd9b8a8a514e0
DielsonFreitas <dielson.freitas@hotmail.com>
2026-08-12 22:33:08 -0300

    Add files via upload

---
+++ Aula_4.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	// Exercicio 1
+	
+	
+	int primeiro, segundo, aux;
+	
+	printf("Digite o primeiro valor: ");
+	scanf("%d", &primeiro);
+	
+	printf("Digite o segundo valor: ");
+	scanf("%d", &segundo);
+	
+	aux = primeiro;
+	primeiro = segundo;
+	segundo = aux;
+	
+	printf("%d \n %d", primeiro, segundo);
+	
+	
+	
+	// Exercicio 2
+	
+	
+	double valor;
+	
+	printf("\nDigite o valor: ");
+	scanf("%lf", &valor);
+	
+	printf("Valor em notação cientifica: %lf", valor);
+	
+	
+	// Exercicio 3
+	
+	int num_1, num_2, binario;
+	num_2 = 2;
+	
+	printf("\nDigite um numero: ");
+	scanf("%f", &num_1);
+	
+	
+	binario = (num_1 * 10) % num_2;
+	
+	printf("Valor em binario: %f\n", binario);
+	
+	
+	
+	
+	return 0;
+}
